9d652641bd9f9a8984f28721f3a4a4bf794eb4d7
A.R. Jibon <atikurrahaman386@gmail.com>
2025-03-29 03:09:16 +0600

    Enforce authentication on home page
    - Redirects unauthenticated users to `/auth/logout`.
    - Initializes a structured `<main>` container for future content.
    - Improves security and user flow by verifying authentication.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
-export default function Home() {
-  return null;
+import getAuth from "@/lib/auth";
+import { redirect } from "next/navigation";
+
+export default async function Home() {
+  const auth = getAuth();
+
+  if (!(await auth.verify([]))) {
+    redirect("/auth/logout");
+  }
+
+  return <main className="container mx-auto p-5 space-y-5"></main>;
 }
