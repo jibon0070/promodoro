fa45b24225928c04a7af4f6eae5dc21bf5d1aca3
A.R. Jibon <atikurrahaman386@gmail.com>
2025-03-28 11:54:52 +0600

    Load user durations in settings page
    - Fetches user-specific durations from the database.
    - Provides default values if no records exist.
    - Displays the `Durations` component for user adjustments.

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,17 +1,27 @@
 import { Metadata } from "next";
+import Durations from "./_components/durations/client";
 import getAuth from "@/lib/auth";
 import { redirect } from "next/navigation";
+import db from "@/db";
 
 export const metadata: Metadata = {
   title: "Settings | Promodoro",
   description: "Settings for your account",
 };
+
 export default async function Settings() {
   const auth = getAuth();
 
   if (!(await auth.verify([]))) {
     redirect("/");
   }
+
+  const payload = await auth.getPayload();
+
+  const durationsDefaultValues = await db.query.DurationsModel.findFirst({
+    where: (model, { eq }) => eq(model.userId, payload.id),
+    columns: { promodoro: true, shortBreak: true, longBreak: true },
+  }).then((row) => row || { promodoro: 25, shortBreak: 5, longBreak: 15 });
 
   return (
     <main className="container mx-auto space-y-5 p-5">
