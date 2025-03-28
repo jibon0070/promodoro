2e3a6e472ac7abc3fd64ef70369f1f085dc19d91
A.R. Jibon <atikurrahaman386@gmail.com>
2025-03-28 11:53:41 +0600

    Add settings page with authentication check
    - Creates a new settings page with metadata.
    - Verifies user authentication before rendering content.
    - Redirects unauthenticated users to the home page.

---
+++ src/app/settings/page.tsx
@@ -0,0 +1,20 @@
+import { Metadata } from "next";
+import getAuth from "@/lib/auth";
+import { redirect } from "next/navigation";
+
+export const metadata: Metadata = {
+  title: "Settings | Promodoro",
+  description: "Settings for your account",
+};
+export default async function Settings() {
+  const auth = getAuth();
+
+  if (!(await auth.verify([]))) {
+    redirect("/");
+  }
+
+  return (
+    <main className="container mx-auto p-5">
+    </main>
+  );
+}
