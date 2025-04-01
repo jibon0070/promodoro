4f1c3a62f2f691b7f18deffe06e387a08ae468ca
A.R. Jibon <atikurrahaman386@gmail.com>
2025-04-02 01:26:59 +0600

    Implement `Timer` component with event tracking UI
    - Fetches current event state using `react-query`.
    - Supports changing event types and toggling pause/resume.
    - Renders a visual timer using `<canvas>`.
    - Ensures real-time updates for an interactive experience.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { Toaster } from "@/components/ui/sonner";
 import "./globals.css";
 import Providers from "./providers";
+import AskForNotification from "@/components/ui/ask-for-notification";
 
 export default function RootLayout({
   children,
@@ -11,6 +12,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <Toaster />
+        <AskForNotification />
         <Providers>{children}</Providers>
       </body>
     </html>
