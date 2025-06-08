3f88f1dfb874751f089f978acdff9cbf4c1cc5c9
A.R. Jibon <atikurrahaman386@gmail.com>
2025-06-09 03:02:18 +0600

    added backup script

---
+++ backup.sh
@@ -0,0 +1,18 @@
+server=promodoro
+folder="~/promodoro"
+backupname="backup-$(date +%Y-%m-%d-%H:%M).zip"
+
+ssh $server <<EOF
+cd $folder
+docker compose down
+zip -r $backupname storage
+docker compose up -d
+EOF
+
+mkdir -p backups
+
+scp "$server:$folder/$backupname" "backups/$backupname"
+
+ssh $server <<EOF
+rm $folder/$backupname
+EOF
